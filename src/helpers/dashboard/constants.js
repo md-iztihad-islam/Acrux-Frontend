b9cb946404be710b2418fbf53b87a9f9ebd0cd43
Md. Iztihad Islam <iiztihad@gmail.com>
2026-01-14 22:02:30 +0600

    Frontend Version-2.0.0.1

--- src/helpers/dashboard/constants.js
+++ src/helpers/dashboard/constants.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://api.acruxshop.com/api/v1";
-export const API_URL = "http://localhost:9001/api/v1";
+export const API_URL = "https://api.acruxshop.com/api/v1";
+// export const API_URL = "http://localhost:9001/api/v1";
