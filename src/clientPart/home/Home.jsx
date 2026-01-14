84dbe4856f109b71449a74da444fe5e6b8a2fa2b
Md. Iztihad Islam <iiztihad@gmail.com>
2026-01-14 21:59:46 +0600

    Frontend Version-2.0.0.1

--- src/clientPart/home/Home.jsx
+++ src/clientPart/home/Home.jsx
@@ -1,10 +1,7 @@
 import HeroSection from "@/components/clientPart/HeroSection";
-import ProductShowcase from "@/components/clientPart/ProductShowcase";
-import CountdownTimer from "@/components/clientPart/CountdownTimer";
 import PricingCards from "@/components/clientPart/PricingCard";
 import OrderForm from "@/components/clientPart/OrderForm";
 import BenefitsSection from "@/components/clientPart/BenefitSection";
-import CustomerReviews from "@/components/clientPart/CustomerReview";
 import Footer from "@/components/clientPart/Footer";
 
 
@@ -12,11 +9,9 @@ function Home() {
     return (
         <div className="min-h-screen ">
             <HeroSection />
-            {/* <ProductShowcase /> */}
             <PricingCards />
             <OrderForm />
             <BenefitsSection />
-            {/* <CustomerReviews /> */}
             <Footer />
         </div>
     );
